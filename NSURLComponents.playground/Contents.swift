5123fbf45c114b41b583d291cfc9a88bbd9e7a6a
Andres Kwan <andreskwan@gmail.com>
2016-08-24 05:31:52 -0500

    [playground] - [NSURLComponents] - [adding queryItems]

--- NSURLComponents.playground/Contents.swift
+++ NSURLComponents.playground/Contents.swift
@@ -8,5 +8,12 @@ let components = NSURLComponents()
 components.scheme = "https"
 components.host = "udacity.com"
 components.path = "/nanodegree"
+components.queryItems = [NSURLQueryItem]()
+
+let queryItem1 = NSURLQueryItem(name: "method", value: "flickr.photos.search")
+let queryItem2 = NSURLQueryItem(name: "api_key", value: "1234")
+
+components.queryItems!.append(queryItem1)
+components.queryItems!.append(queryItem2)
 
 print(components.URL)
