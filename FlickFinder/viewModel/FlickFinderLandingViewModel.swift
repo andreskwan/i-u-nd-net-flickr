6bf04e1d5eceffeca09dbd7477465daa0786cc56
Andres Kwan <andreskwan@gmail.com>
2016-08-28 23:51:46 -0500

    [refactor] - [Constants] - [validation o latitudeText] - tuples parameters names

--- FlickFinder/viewModel/FlickFinderLandingViewModel.swift
+++ FlickFinder/viewModel/FlickFinderLandingViewModel.swift
@@ -11,16 +11,22 @@ import ReactiveKit
 
 class FlicFinderLandingViewModel {
     let latitudeText: Observable<String?> = Observable("")
+    let longitudeText: Observable<String?> = Observable("")
     let isValidLatitude = Observable<Bool>(false)
     
     init() {
+        
+        /*
+         How to avoid repeting code?
+         what if I need to reuse a map
+         */
         latitudeText
             .map{ (latNumber) -> Bool in
                 guard let latNumber = latNumber where latNumber.characters.count > 0 else {
                     return false
                 }
-                let isLowValid = Constants.Flickr.SearchLatRange.minLat <= Double(latNumber)
-                let isHighValid = Constants.Flickr.SearchLatRange.maxLat >= Double(latNumber)
+                let isLowValid = Constants.Flickr.SearchLatRange.min <= Double(latNumber)
+                let isHighValid = Constants.Flickr.SearchLatRange.max >= Double(latNumber)
                 return isLowValid && isHighValid
             }.bindTo(isValidLatitude)
     }
